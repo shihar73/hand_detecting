3452822ed5349e22168878fb5b0d958ba41586bb
shihar <shiharbinsideeque73@gmail.com>
2021-05-08 22:36:28 +0530

    trying to change volume

---
+++ volume_control.py
@@ -0,0 +1,46 @@
+import cv2
+import time
+import hand_traking_module as htm
+import math
+
+wCam, hCam = 640, 480
+cap = cv2.VideoCapture(0)
+cap.set(2, wCam)
+cap.set(3, hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.7)
+
+
+
+while True:
+    success, img = cap.read()
+    detector.finHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        print(length)
+
+        if length < 50:
+            cv2.circle(img, (x2, y2), 10, (0, 255, 0), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS:{int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1,
+                (255, 0, 0), 3)
+
+    cv2.imshow('img', img)
+    cv2.waitKey(1)
